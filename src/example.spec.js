f36dd52d32d7962f13ec01fcb998b7c301c8e3b2
Harpal <harpalassi@gmail.com>
2019-10-20 16:10:37 -0500

    ran audit fix

--- src/example.spec.js
+++ src/example.spec.js
@@ -8,8 +8,20 @@ sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
 
-describe('Example', () => {
-  it('should x when y', () => {
-    expect(2).to.be.eql(2);
+describe('My bowling scorer', () => {
+  beforeEach(() => {
+    // This example test suite is for a nudge in the right direction but feel free
+    // change the shape of the specs or even move in a completely different direction that you feel best for
+    // the assignment. Make sure to break down the issue and keep things organized.
+  });
+
+  const myScoringAlgorithm = frames => {};
+
+  it('calculates correct score provided a game', () => {
+    const frames = [[0, 1], [0, 8], [8, 1]];
+
+    let score = myScoringAlgorithm(frames);
+
+    expect(score).to.be.eql(18);
   });
 });
