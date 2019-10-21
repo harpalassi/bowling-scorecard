ca248abcbeef0c46a3fef08f71d9cfbbdf740e09
Harpal <harpalassi@gmail.com>
2019-10-20 22:56:32 -0500

    added spare algorithm

--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,31 @@
+// create Bowling class object to intialize gamescoring
 export class Bowling {
   constructor() {
-    this.score = 0;
+    // rolls is the array of total rolls in the entire game
+    this.rolls = [];
+  }
+  // this method takes in the amount of pins hit and will put them into the rolls array
+  addRoll(pins) {
+    this.rolls.push(pins);
+  }
+  //main scoring algorithm to calculate total score for the game
+  getScore() {
+    let score = 0;
+    //iterate through two different indices in the rolls array which will include 10 frame arrays.
+    for (
+      let frameIndex = 0, rollIndex = 0;
+      frameIndex < 10;
+      frameIndex++, rollIndex += 2
+    ) {
+      // the first roll of the frame will be added to the score and so will the second roll by increasing the index
+      score += this.rolls[rollIndex];
+      score += this.rolls[rollIndex + 1];
+
+      // if a spare is rolled within a frame (10 pins total), add the score of the following 1st index of frame
+      if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
+        score += this.rolls[rollIndex + 2];
+      }
+    }
+    return score;
   }
 }
--- src/bowling.spec.js
+++ src/bowling.spec.js
@@ -10,17 +10,44 @@ sinonStubPromise(sinon);
 global.fetch = require('node-fetch');
 
 describe('Bowling Scorecard', () => {
+  let bowling = Bowling;
   beforeEach(() => {
+    bowling = new Bowling();
     // This example test suite is for a nudge in the right direction but feel free
     // change the shape of the specs or even move in a completely different direction that you feel best for
     // the assignment. Make sure to break down the issue and keep things organized.
   });
 
-  const myScoringAlgorithm = frames => {};
+  let addRolls = (pins, times) => {
+    for (let i = 0; i < times; i++) {
+      bowling.addRoll(pins);
+    }
+  };
 
-  it('all gutters', () => {
-    let bowling = new Bowling();
+  // const myScoringAlgorithm = frames => {};
+
+  it('it calculates total with all gutterballs', () => {
+    addRolls(0, 20);
+    expect(bowling.getScore()).to.be.eql(0);
   });
+
+  it('it calculates total with all fours', () => {
+    addRolls(4, 20);
+    expect(bowling.getScore()).to.be.eql(80);
+  });
+
+  it('it calculates total with a spare and all gutterballs', () => {
+    addRolls(5, 2);
+    addRolls(0, 18);
+    expect(bowling.getScore()).to.be.eql(10);
+  });
+
+  it('it calculates total with a spare and all threes', () => {
+    addRolls(5, 2);
+    addRolls(3, 18);
+    expect(bowling.getScore()).to.be.eql(67);
+  });
+
   // it('calculates correct score provided a game', () => {
   //   const frames = [[0, 1], [0, 8], [8, 1]];
 
