4f545b518297bbb7e1ea9a84c63da3895f73c1a3
Harpal <harpalassi@gmail.com>
2019-10-20 20:43:09 -0500

    created bowling class

---
+++ src/bowling.js
@@ -0,0 +1,5 @@
+export class Bowling {
+  constructor() {
+    this.score = 0;
+  }
+}
--- src/bowling.spec.js
+++ src/bowling.spec.js
@@ -2,13 +2,14 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
+import { Bowling } from './bowling.js';
 
 chai.use(sinonChai);
 sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
 
-describe('My bowling scorer', () => {
+describe('Bowling Scorecard', () => {
   beforeEach(() => {
     // This example test suite is for a nudge in the right direction but feel free
     // change the shape of the specs or even move in a completely different direction that you feel best for
@@ -17,11 +18,14 @@ describe('My bowling scorer', () => {
 
   const myScoringAlgorithm = frames => {};
 
-  it('calculates correct score provided a game', () => {
-    const frames = [[0, 1], [0, 8], [8, 1]];
-
-    let score = myScoringAlgorithm(frames);
-
-    expect(score).to.be.eql(18);
+  it('all gutters', () => {
+    let bowling = new Bowling();
   });
+  // it('calculates correct score provided a game', () => {
+  //   const frames = [[0, 1], [0, 8], [8, 1]];
+
+  //   let score = myScoringAlgorithm(frames);
+
+  //   expect(score).to.be.eql(18);
+  // });
 });
